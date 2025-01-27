de38e86c7dd5cee739e99d60088277aca6c0bcfc
Cauã de Lima Araújo <20231144010003@ifrn.local>
2025-01-27 15:39:37 -0300

    feat(repositories): Adicionando métodos de consulta por id, email e criação de usuários

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -5,5 +5,18 @@ const UserRepository = {
     async findAll() {
         const rows = await DBInterface.query('SELECT * FROM users');
         return rows.map((row => new User(row.id, row.name, row.email, row.password_hash)));
+    },
+    async findAllById(id) {
+        const rows = await DBInterface.query('SELECT * FROM users WHERE id = ?', [id]);
+        return rows.map((row) => new User(row.id, row.name, row.email, row.password_hash));
+    },
+    async findByEmail(email) {
+        const rows = await DBInterface.query('SELECT * FROM users WHERE email = ?', [email]);
+        return rows.map((row) => new User(row.id, row.name, row.email, row.password_hash));
+    },
+    async create(user) {
+        const result = await DBInterface.query('INSERT ON TABLE users (id, email, password_hash) VALUES (?,?,?)', [user.id, user.email, user.password_hash]);
+        user.id = result.insertId;
+        return
     }
 }
