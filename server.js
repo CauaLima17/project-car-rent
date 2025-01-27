5ec996aa9d1b4803ad85d2e75f94038e5336632a
Cauã de Lima <caua-l1@outlook.com>
2025-01-27 20:25:56 -0300

    feat(server): Adicionando rotas para usuários e autenticação

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import app from "./src/app.js";
 import dotenv from 'dotenv';
 import CarRouter from "./src/routers/CarRouter.js";
+import UserRouter from "./src/routers/UserRouter.js";
+import AuthRouter from "./src/routers/AuthRouter.js";
 
 dotenv.config();
 
-app.use('/carros', CarRouter)
+app.use('/carros', CarRouter);
+app.use('/usuarios', UserRouter);
+app.use('/auth', AuthRouter);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Servidor conectado com sucesso')
