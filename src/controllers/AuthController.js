863211b29eef256900f79c0f2555902bbaa6e072
Cauã de Lima <caua-l1@outlook.com>
2025-01-27 20:40:19 -0300

    fix(controller): Corrijindo erro undefined no hash de senha do login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -7,18 +7,17 @@ const AuthController = {
       try {
         const data = req.body;
         const user = await UserRepository.findByEmail(data.email);
-
         if (user.length === 0) {
           return res.status(404).json({ error: "Usuário não encontrado." })
         }
-
-        const passwordMatch = await bcrypt.compare(data.password, user[0].password_hash);
-
+        
+        const passwordMatch = await bcrypt.compare(data.password, user[0].getPasswordHash());
+        
         if (!passwordMatch) {
           return res.status(400).json({ error: "Senha incorreta." });
         }
 
-        const token = jwt.sign({ id: user[0].id, username: user[0].name, email: user[0].email }, process.env.TOKEN_KEY || 'JWT-API-KEY-HERE', {expiresIn: '1h'})
+        const token = jwt.sign({ id: user[0].getId(), username: user[0].getName(), email: user[0].getEmail() }, process.env.TOKEN_KEY || 'JWT-API-KEY-HERE', {expiresIn: '1h'})
         return res.status(200).json(token);
         
       } catch (error) {
