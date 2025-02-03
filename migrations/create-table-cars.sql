bd5aa5004d446d080c928a70a1a173b484362fc0
Cauã de Lima Araújo <20231144010003@ifrn.local>
2025-02-03 15:50:54 -0300

    feat(migrations): Adicionando novas tabelas (orders, employee) ao banco

--- migrations/create-table-cars.sql
+++ migrations/create-table-cars.sql
@@ -1,10 +1,10 @@
 CREATE TABLE cars ( 
     id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT DEFAULT NULL,
     model VARCHAR(100) NOT NULL,
     brand VARCHAR(100) NOT NULL,
-    is_rented BOOLEAN DEFAULT 0,
+    status ENUM('Disponível', 'Reservado', 'Alugado') DEFAULT 'Disponível' NOT NULL,
     category_id INT NOT NULL,
-    user_id INT DEFAULT NULL,
     description VARCHAR(400) NOT NULL, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (category_id) REFERENCES categories(id),
---
+++ migrations/create-table-orders.sql
@@ -0,0 +1,14 @@
+CREATE TABLE orders ( 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    car_id INT NOT NULL,
+    user_id INT NOT NULL,
+    payment_method VARCHAR(100) NOT NULL,
+    pick_up_location VARCHAR(300) NOT NULL,
+    drop_off_location VARCHAR(300) NOT NULL,
+    pick_up_date DATETIME NOT NULL,
+    drop_off_date DATETIME NOT NULL,
+    order_description INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY (car_id) REFERENCES cars(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
