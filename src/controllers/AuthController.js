e7a44a7e82d2f7da3da4984cbfb998d751432b6b
Cauã de Lima Araújo <20231144010003@ifrn.local>
2025-02-03 15:54:47 -0300

    fix(controllers): Corrigindo o registro de usuários

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -23,25 +23,6 @@ const AuthController = {
       } catch (error) {
         return res.status(500).json({ error: "ERR-Login: " + error });
       }
-    },
-
-    async register(req, res) {
-      try {
-        const data = req.body;
-        const user = await UserRepository.findByEmail(data.email);
-
-        if (user.length > 0) {
-          return res.status(409).json({ error: "Email já cadastrado." });
-        }
-
-        const password_hash = await bcrypt.hash(data.password, 8);
-        data.password_hash = password_hash;
-
-        await UserRepository.create(data);
-        return res.status(200).json({ message: "Usuário criado com sucesso." });
-      } catch (error) {
-        return res.status(500).json({ error: "ERR-Registro: " + error });
-      }
     }
 }
 
