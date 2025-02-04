cd51c4be55f5eac41733c046db7fdae18bcb1020
BRUNO GUSTAVO LOPES DE SOUZA <20231144010008@ifrn.local>
2025-02-04 17:50:53 -0300

    Adicionado criação de pedidos na parte de repositórios e controllers

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -8,6 +8,15 @@ const OrderController = {
     } catch (err) {
       res.status(500).json({error: 'Erro na busca por pedidos: ' + err})
     }
+  },
+  async create(req, res) {
+    const data = req.body;
+    try {
+      await OrderRepository.create(data)
+      return res.status(200).json({message: 'Pedido criado com sucesso'});
+    } catch (err) {
+      return res.status(500).json({ error: 'Erro ao criar o pedido: ' + err })
+    }
   }
 }
 
